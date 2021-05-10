8ddc497653e0567054380d2777ff37580631b540
Kyrie Lee <867192428@qq.com>
2021-05-10 15:24:01 +0800

    initial commit

---
+++ src/main/java/cn/ecnu/damai/DamaiApplication.java
@@ -0,0 +1,13 @@
+package cn.ecnu.damai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamaiApplication.class, args);
+    }
+
+}
