28c310703f72df7298fa620f0dc61362c750a29a
zzzz76 <15079084006@163.com>
2021-05-13 22:34:31 +0800

    Add get levels by showId

---
+++ src/test/java/cn/ecnu/damai/controller/AdminControllerTest.java
@@ -0,0 +1,27 @@
+package cn.ecnu.damai.controller;
+
+import cn.ecnu.damai.entity.Program;
+import cn.ecnu.damai.util.DamaiCrawler;
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DamaiCrawlerTest {
+
+    private DamaiCrawler damaiCrawler = new DamaiCrawler();
+
+    @Test
+    public void crawl() {
+        Program program = new Program();
+        String errStatus = damaiCrawler.synCrawl("644431227376", program);
+        if (errStatus == null) {
+            System.out.println(JSON.toJSONString(program));
+        } else {
+            System.out.println(errStatus);
+        }
+    }
+}
---
+++ src/test/java/cn/ecnu/damai/service/LevelServiceTest.java
@@ -0,0 +1,4 @@
+package cn.ecnu.damai.service;
+
+public class LevelServiceTest {
+}
